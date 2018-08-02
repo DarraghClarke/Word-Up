d50ead91b9411eb102ad70d9e2941d16ad7e4ef7
DarraghClarke <darragh.clarke@ucdconnect.ie>
2018-08-02 23:19:47 +0100

    Initial push
    This is really just a text box , its as bare bones as this project can be

---
+++ Word/Controller.java
@@ -0,0 +1,18 @@
+package Word;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private TextArea textArea;//where the text is input
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
