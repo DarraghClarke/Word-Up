d045723887bee3c2e20d91fa764be494167124a9
DarraghClarke <darragh.clarke@ucdconnect.ie>
2018-08-09 22:27:32 +0100

    FXRibbon Added
    Currently  basic implementation without proper Buttons and icons , but base code started

--- Main.java
+++ Main.java
@@ -2,9 +2,14 @@ package Word;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import static com.sun.org.apache.xalan.internal.utils.SecuritySupport.getResourceAsStream;
 
 public class Main extends Application {
 
@@ -12,7 +17,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Word Up");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.getIcons().add( new Image( Main.class.getResourceAsStream( "placeholder.png" )));
+        //primaryStage.setScene(new Scene(root, 1200, 900));
+        primaryStage.setScene(new Scene(root));
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
         primaryStage.show();
     }
 
